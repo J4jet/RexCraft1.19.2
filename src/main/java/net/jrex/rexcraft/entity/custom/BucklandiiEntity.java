1706f0db1618ab460d39599a1cab8982e3b4bd5b
Jasaurus <jrex4jet@gmail.com>
2023-07-05 01:58:08 -0400

    changed some goals

--- src/main/java/net/jrex/rexcraft/entity/custom/BucklandiiEntity.java
+++ src/main/java/net/jrex/rexcraft/entity/custom/BucklandiiEntity.java
@@ -90,10 +90,10 @@ public class BucklandiiEntity extends TamableAnimal implements IAnimatable {
 
             this.goalSelector.addGoal(1, new FloatGoal(this));
             this.goalSelector.addGoal(1, new SitWhenOrderedToGoal(this));
-            this.goalSelector.addGoal(2, new MeleeAttackGoal(this, 2.0D, false));
             this.goalSelector.addGoal(2, new FollowOwnerGoal(this, 2.0D, 10.0F, 6.0F, false));
+            this.goalSelector.addGoal(2, new FollowParentGoal(this, 1.1D));
+            this.goalSelector.addGoal(3, new MeleeAttackGoal(this, 2.0D, false));
             this.goalSelector.addGoal(3, new BreedGoal(this, 1.0D));
-            this.goalSelector.addGoal(4, new FollowParentGoal(this, 1.1D));
             this.goalSelector.addGoal(4, new LookAtPlayerGoal(this, Player.class, 6.0F));
             this.goalSelector.addGoal(4, new WaterAvoidingRandomStrollGoal(this, 1.0D));
             this.goalSelector.addGoal(5, new RandomLookAroundGoal(this));
